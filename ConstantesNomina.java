392d12c84d52f8e0e8e6dc3dcb55d9cb3027dcff
MrmasterNico <juanicolasbuitragogarzo@gmail.com>
2026-04-23 09:37:50 -0500

    Create ConstantesNomina.java

---
+++ ConstantesNomina.java
@@ -0,0 +1,9 @@
+package Empresa;
+
+public final class ConstantesNomina {
+    public static final int ANIO_ACTUAL = 2026;
+    public static final double TASA_PRESTACIONES = 0.10;
+
+    private ConstantesNomina() {
+    }
+}
